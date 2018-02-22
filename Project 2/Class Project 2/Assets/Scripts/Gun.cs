6383c6287e3dda02d63b5fbdb3b000d81440b46a
Marauder4294 <marauder42@ymail.com>
2018-02-21 21:43:50 -0700

    Project 2
    Initial Commit

---
+++ Project 2/Class Project 2/Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    public float fireRate;
+    protected float lastFireTime;
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+    void Start()
+    {
+        lastFireTime = Time.time - 10;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+
+    }
+
+    protected void Fire()
+    {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponentInChildren<Animator>().Play("Fire");
+        }
+        GetComponentInChildren<Animator>().Play("Fire");
+    }
+}
