7bac948556cbba35885479a09e8d95a85f66d557
Marauder4294 <marauder42@ymail.com>
2018-03-05 00:05:01 -0700

    A4

---
+++ A4/FPS Shooting Gallery/Assets/Scripts/Ammo.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ammo : MonoBehaviour
+{
+    public Text BearAmmoText;
+    public Text PingPongBallAmmoText;
+    public Text CarAmmoText;
+
+    public GameObject Bear;
+    public GameObject PingPong;
+    public GameObject Car;
+
+    public Transform GunBarrel;
+
+    public ushort BearAmmo;
+    public ushort PingPongBallAmmo;
+    public ushort CarAmmo;
+
+    public float BearFireFrequency;
+    public float PingPongBallFireFrequency;
+    public float CarFireFrequency;
+
+    public float BearProjectileSpeed;
+    public float PingPongProjectileSpeed;
+    public float CarProjectileSpeed;
+
+    public RawImage BearUnderline;
+    public RawImage PingPongUnderline;
+    public RawImage CarUnderline;
+
+    public AudioClip BearSound;
+    public AudioClip PingPongSound;
+    public AudioClip CarSound;
+    public AudioClip NoAmmo;
+    public AudioClip SwitchAmmo;
+
+    private string equippedAmmo; // "Bear", "PingPong", "Car"
+    private float timeToFire;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        BearAmmoText.text = "x " + BearAmmo;
+        PingPongBallAmmoText.text = "x " + PingPongBallAmmo;
+        CarAmmoText.text = "x " + CarAmmo;
+
+        PingPongUnderline.color = Color.white;
+        CarUnderline.color = Color.white;
+
+        equippedAmmo = "Bear";
+        BearUnderline.color = Color.red;
+        timeToFire = BearFireFrequency;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeToFire = timeToFire - Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timeToFire <= 0)
+        {
+            if (equippedAmmo == "Bear")
+            {
+                if (BearAmmo <= 0)
+                {
+                    NoAmmoSound();
+                }
+                else
+                {
+                    GameObject projectile = Instantiate(Bear, new Vector3(GunBarrel.position.x, GunBarrel.position.y, GunBarrel.position.z), GunBarrel.rotation);
+                    projectile.GetComponent<Rigidbody>().velocity = -projectile.transform.up * BearProjectileSpeed;
+
+                    BearAmmo--;
+                    BearAmmoText.text = "x " + BearAmmo;
+                    timeToFire = BearFireFrequency;
+
+                    GetComponent<AudioSource>().PlayOneShot(BearSound);
+
+                    Destroy(projectile, 10.0f);
+                }
+                
+            }
+            else if (equippedAmmo == "PingPong")
+            {
+                if (PingPongBallAmmo <= 0)
+                {
+                    NoAmmoSound();
+                }
+                else
+                {
+                    GameObject projectile = Instantiate(PingPong, new Vector3(GunBarrel.position.x, GunBarrel.position.y, GunBarrel.position.z), GunBarrel.rotation);
+                    projectile.GetComponent<Rigidbody>().velocity = -projectile.transform.up * PingPongProjectileSpeed;
+
+                    GetComponent<AudioSource>().PlayOneShot(PingPongSound);
+
+                    PingPongBallAmmo--;
+                    PingPongBallAmmoText.text = "x " + PingPongBallAmmo;
+                    timeToFire = PingPongBallFireFrequency;
+
+                    Destroy(projectile, 2.0f);
+                }
+            }
+            else if (equippedAmmo == "Car")
+            {
+                if (CarAmmo <= 0)
+                {
+                    NoAmmoSound();
+                }
+                else
+                {
+                    GameObject projectile = Instantiate(Car, new Vector3(GunBarrel.position.x, GunBarrel.position.y, GunBarrel.position.z), GunBarrel.rotation);
+                    projectile.GetComponent<Rigidbody>().velocity = -projectile.transform.up * CarProjectileSpeed;
+
+                    CarAmmo--;
+                    CarAmmoText.text = "x " + CarAmmo;
+                    timeToFire = CarFireFrequency;
+
+                    GetComponent<AudioSource>().PlayOneShot(CarSound);
+
+                    StartCoroutine(ChangeCarSize(projectile));
+
+                    Destroy(projectile, 10.0f);
+                }
+            }
+        }
+        else if (Input.GetKey(KeyCode.Keypad1) && equippedAmmo != "Bear")
+        {
+            ChangeAmmoSound();
+
+            BearUnderline.color = Color.red;
+            PingPongUnderline.color = Color.white;
+            CarUnderline.color = Color.white;
+
+            equippedAmmo = "Bear";
+            timeToFire = 0;
+        }
+        else if (Input.GetKey(KeyCode.Keypad2) && equippedAmmo != "PingPong")
+        {
+            ChangeAmmoSound();
+
+            BearUnderline.color = Color.white;
+            PingPongUnderline.color = Color.red;
+            CarUnderline.color = Color.white;
+
+            equippedAmmo = "PingPong";
+            timeToFire = 0;
+        }
+        else if (Input.GetKey(KeyCode.Keypad3) && equippedAmmo != "Car")
+        {
+            ChangeAmmoSound();
+
+            BearUnderline.color = Color.white;
+            PingPongUnderline.color = Color.white;
+            CarUnderline.color = Color.red;
+
+            equippedAmmo = "Car";
+            timeToFire = 0;
+        }
+    }
+
+    void NoAmmoSound()
+    {
+        GetComponent<AudioSource>().PlayOneShot(NoAmmo);
+    }
+
+    void ChangeAmmoSound()
+    {
+        GetComponent<AudioSource>().PlayOneShot(SwitchAmmo);
+    }
+
+    IEnumerator ChangeCarSize(GameObject projectile)
+    {
+        yield return new WaitForSeconds(0.25f);
+        projectile.transform.localScale = new Vector3(.6f, .6f, .6f);
+    }
+}
---
+++ A4/FPS Shooting Gallery/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float horizontalSensitivity;
+    public float verticalSensitivity;
+    public float mouseSensitivity;
+    public bool mouseInversion;
+    private short inversion;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+
+        if (mouseInversion == true)
+        {
+            inversion = 1;
+        }
+        else
+        {
+            inversion = -1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseVertical = inversion * Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(0, mouseHorizontal, 0, Space.Self);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        Camera.main.transform.Rotate(mouseVertical, 0, 0);
+        Camera.main.transform.eulerAngles = new Vector3(Camera.main.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * verticalSensitivity);// , Space.Self);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * verticalSensitivity);// , Space.Self);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSensitivity);// , Space.Self);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * horizontalSensitivity);// , Space.Self);
+        }
+    }
+}
